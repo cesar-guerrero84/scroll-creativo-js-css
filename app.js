084e671b0b88abab142ca1e3d253d5950c4520e5
cesar guerrero <guerrerofernandez007@gmail.com>
2020-09-30 16:43:48 -0500

    el primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let progress = document.getElementById("progress-bar");
+let totalHeight = document.body.scrollHeight - window.innerHeight; window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%"
+}
